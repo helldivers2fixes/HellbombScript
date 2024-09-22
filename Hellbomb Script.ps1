5d0cb2092003ec06c29cebea3b3607a55884769a
helldivers2fixes <166264070+helldivers2fixes@users.noreply.github.com>
2024-09-22 15:58:13 -0500

    Tweak Dual-Stack Error-handling

--- Hellbomb Script.ps1
+++ Hellbomb Script.ps1
@@ -534,7 +534,7 @@ Function Test-ClientDnsConfig {
     
     Write-Host "`nCHECKING IPV4 DNS..." -ForegroundColor Cyan
     # Print and test DNS servers for IPv4
-    If ((-not ([string]::IsNullOrEmpty($dnsServersIPv4))) -and ($dnsServersIPv4.PSObject.Properties.Name -contains 'ServerAddresses')) {
+    If ((-not ([string]::IsNullOrEmpty($dnsServersIPv4))) -and ($dnsServersIPv4 | Get-Member -Name 'ServerAddresses')) {
         Write-Host "[PASS]" -ForegroundColor Green -NoNewline
         Write-Host " Detected IPv4 DNS servers:"
         $dnsServersIPv4.ServerAddresses | ForEach-Object { Write-Host "       $_"
@@ -549,7 +549,7 @@ Function Test-ClientDnsConfig {
 
     # Print and test DNS servers for IPv6
     Write-Host "`nCHECKING IPV6 DNS..." -ForegroundColor Cyan
-    If ((-not ([string]::IsNullOrEmpty($dnsServersIPv6.ServerAddresses))) -and ($dnsServersIPv6.PSObject.Properties.Name -contains 'ServerAddresses')) {
+    If ((-not ([string]::IsNullOrEmpty($dnsServersIPv6.ServerAddresses))) -and ($dnsServersIPv6 | Get-Member -Name 'ServerAddresses')) {
         Write-Host "[PASS]" -ForegroundColor Green -NoNewline
         Write-Host ' Detected IPv6 DNS server(s):'
         $dnsServersIPv6.ServerAddresses | ForEach-Object { Write-Host "       $_" }
