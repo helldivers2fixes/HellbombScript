a2739737d7dba4cc58b7daf1ac9fbcfdd0121f4f
Luna <34936608+Plootie@users.noreply.github.com>
2026-09-08 23:03:34 +0100

    Shorten empty launch option output

--- Hellbomb Script.ps1
+++ Hellbomb Script.ps1
@@ -1199,7 +1199,7 @@ Function Show-GameLaunchOptions {
         $HD2LaunchOptions = $HD2ConfigData["LaunchOptions"]
         if([string]::IsNullOrWhiteSpace($HD2LaunchOptions))
         {
-            Write-Host "No launch options currently in use."
+            Write-Host "No launch options detected."
         }
         Else
         {
